9afff73de3e2e93fa76d8d9912061bb6a5a3bf2a
anurag maithani <anuragmaithani.official@gmaiil.com>
2020-08-13 17:15:21 +0530

    lol commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -17,7 +17,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using-self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -27,7 +27,7 @@ class UserProfileManager(BaseUserManager):
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(self._db)
+        user.save(using=self._db)
 
         return user
 
