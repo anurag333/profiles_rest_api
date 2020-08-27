b8ef953cae76df750763cdebc25be3a2d0a5500c
anurag maithani <anuragmaithani.official@gmaiil.com>
2020-08-27 14:03:07 +0530

    added filters for searching

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -10,6 +10,7 @@ from profiles_api import models
 
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -106,3 +107,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
